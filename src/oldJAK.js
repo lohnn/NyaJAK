a7bf05b7b10903c668254ac0e27d51338aa3d1f3
Johannes Löhnn <jlohnn@gmail.com>
2015-07-27 23:00:52 +0200

    Old JAK updated.

--- src/oldJAK.js
+++ src/oldJAK.js
@@ -16,6 +16,13 @@ var OldJAK = React.createClass({
         var optimal_u_kvot = 0.9;
 
         this.amortering = (values.amount / (values.time * 12));
+        this.savings = this.amortering;
+
+        this.fee = (0.000875 * values.amount);
+
+        this.månadsbetalning = this.amortering + this.savings + this.fee;
+
+        this.efterAmortering = this.savings * values.time * 12;
 
         var M26 = 100 / (optimal_u_kvot / values.u_kvot);
         var M43 = 100 / (optimal_u_kvot / ((optimal_u_kvot / (values.state.maxTime - values.state.minTime)) * (values.time - values.state.minTime)));
@@ -23,14 +30,6 @@ var OldJAK = React.createClass({
         var N38 = values.förspar * (1 / (values.u_kvot / optimal_u_kvot));
         var M44 = ((M43 / 100) * ((values.amount / (values.time * 12)) / 2 * ((values.time * 12) + 1) * (values.time * 12)) - (N38));
         var O44 = ((this.amortering / 2 * ((values.time * 12) + 1)) * (values.time * 12));
-        this.savings = ((M44 / O44) * this.amortering);
-        this.savings = (this.savings < 0) ? 0 : this.savings;
-
-        this.fee = (0.000875 * values.amount);
-
-        this.månadsbetalning = this.amortering + this.savings + this.fee;
-
-        this.efterAmortering = this.savings * values.time * 12;
 
         this.sparpoängKvar = (M44 > 0) ? 0 : -M44;
 
