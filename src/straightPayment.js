9075dcc0f7b90fcb9af94b87648319052c24a231
Johannes Löhnn <jlohnn@gmail.com>
2015-08-26 12:19:45 +0200

    Upgraded the algorithm

--- src/straightPayment.js
+++ src/straightPayment.js
@@ -15,10 +15,9 @@ var PaymentMixin = {
 
     calculateStraightPayment: function (values, amortering) {
         var loanCost = {start: 0.00245*(values.amount)};
-        var tempAmount = values.amount;
-        for(var i = 1; i < values.time*12; i++){
-            tempAmount -= amortering;
-        }
+        var tempAmount = values.amount - amortering*(values.time*12 - 1);
+        console.log(tempAmount);
+
         loanCost.end = 0.00245*(tempAmount);
 
         this.setState({
